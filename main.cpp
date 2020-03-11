809bde4fc3756d2c61713b519db76f86626192b1
Eric Kiesow <ekiesow@mail.csuchico.edu>
2020-03-11 12:19:57 -0700

    did not find dead code, added duplicated code

--- main.cpp
+++ main.cpp
@@ -9,10 +9,26 @@ int main()
 {
   int num1, num2 = 0;
 
-    int x,y,z;
+  int x,y,z;
 
-    cin >> x >> y;
-    cout << x + y << endl;
+  cin >> x >> y;
+  cout << x + y << endl;
 
-    return 0;
+  int array_a[4];
+  int array_b[4];
+
+  int sum_a = 0;
+
+  for (int i = 0; i < 4; i++)
+    sum_a += array_a[i];
+
+  int average_a = sum_a / 4;
+
+  int sum_b = 0;
+
+  for (int i = 0; i < 4; i++)
+    sum_b += array_b[i];
+
+  int average_b = sum_b / 4;
+  return 0;
 }
